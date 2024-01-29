01ddce4e61338d1f3a2b2ae7ed75ef4b7c65fae2
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-29 18:38:20 +0600

    Update Header.tsx

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='p-10'>
+      <nav className='flex justify-center'>
+        <Link className='ml-5' href="/">Home</Link>
+        <Link className='ml-5' href="/Category">Category</Link>
+        <Link className='ml-5' href="/AboutUs">About Us</Link>
+      </nav>
+    </header>
   )
 }
 
