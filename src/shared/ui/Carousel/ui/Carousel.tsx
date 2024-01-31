62346bc0b183aa074c4da2a495b67c9e5f7dafa4
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2024-01-31 20:02:37 +0600

    First try to create slider

--- src/shared/ui/Carousel/ui/Carousel.tsx
+++ src/shared/ui/Carousel/ui/Carousel.tsx
@@ -2,7 +2,7 @@
 
 import { Icon } from 'next/dist/lib/metadata/types/metadata-types'
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface IImg{
   id:number;
@@ -14,11 +14,24 @@ interface ICarousel {
 }
 
 const Carousel = ({imgs}:ICarousel) => {
+
+  const [currentImg, setCurrentImg] = useState(1);
+
+  function clickHandler(){
+    let width = 1080;
+    Img.style.marginLeft = -120 + 'px';
+    console.log("Hello")
+    setCurrentImg(currentImg + 1);
+  }
+
   return (
-    <div className="inline-block">
-      <ul className=''>
-        {imgs.map((img) => <Image alt={"Slider"} src={img.src}/>)}
+    <div className="flex flex-col">
+      <ul className='flex overflow-hidden '>
+        <p className='absolute p-10 cursor-pointer right-0' onClick={clickHandler}>##</p>
+        {imgs.map((img) => 
+          <Image alt={"Slider"} id={`Img${img.id}`} src={img.src} className=''/>)}
       </ul>
+
     </div>
   )
 }
